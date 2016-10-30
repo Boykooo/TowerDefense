49eb0f158db611ebc3c22b4ecf949c2c748d8c96
Boykooo <Boykooo@bk.ru>
2016-10-30 19:06:11 +0300

    Добавил проект библиотеки Network. Реализовал класс Message

---
+++ Network/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class Message
+    {
+        public Message(string method, params object[] args)
+        {
+            Method = method;
+            Arguments = args;
+        }
+
+        public string Method { get; }
+        public object[] Arguments { get; }
+    }
+}
