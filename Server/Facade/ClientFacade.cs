cf6acb2f81dfb5b18c1fc9afd978e530e507377a
Boykooo <Boykooo@bk.ru>
2016-10-30 03:09:50 +0300

    Добавил проект библиотеки Proxy (Фасад клиента, Фасад сервера)

---
+++ Server/Facade/ClientFacade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Facade
+{
+    public class ClientFacade
+    {
+        public ClientFacade()
+        {
+
+        }
+    }
+}
---
+++ Server/Facade/ServerFacade.cs
@@ -0,0 +1,27 @@
+﻿using Proxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Facade
+{
+    class ServerFacade : IServerFacade
+    {
+        public ServerFacade()
+        {
+
+        }
+
+        public void SignIn(string login, string password)
+        {
+            Console.WriteLine("Подключился {0}", login);
+        }
+
+        public void SignUp(string login, string password, string mail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
