883f070194ea67e0fbc6e7245aa0785c1a3e7200
Boykooo <Boykooo@bk.ru>
2016-11-07 23:41:12 +0300

    Рефакторинг DBController

--- Server/DB/DBController.cs
+++ Server/DB/DBController.cs
@@ -10,73 +10,12 @@ namespace Server.DB
     {
         public DBController()
         {
-            InitConnection();
+            model = new TDModel();
         }
 
-        private MySqlConnection conn;
+        private TDModel model;
         private object key = new object();
 
-        private void InitConnection()
-        {
-            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
-            builder.Server = "91.202.20.14";
-            builder.Port = 3306;
-            builder.UserID = "root";
-            builder.Password = "root";
-            builder.Database = "tower_defence";
-            conn = new MySqlConnection(builder.ToString());
-        }
-        private void OpenConnection()
-        {
-            lock (key)
-            {
-                try
-                {
-                    conn.Open();
-                }
-                catch
-                {
-                    Console.WriteLine("------   Ошибка при подключении к удаленной бд...    ------");
-                }
-            }
-
-        }
-        private void CloseConnection()
-        {
-            if (conn != null)
-            {
-                conn.Close();
-            }
-        }
-
-        public void CheckConnect()
-        {
-
-            try
-            {
-                conn.Open();
-                string sql = "SELECT * FROM user_reg_data";
-                MySqlCommand script = new MySqlCommand(sql, conn);
-                MySqlDataReader data = script.ExecuteReader();
-
-                data.Read();
-
-                int id = (int)data["ID"];
-                string login = data["Login"].ToString();
-                string password = data["Password"].ToString();
-                string mail = data["Mail"].ToString();
-                string reg_date = data["Reg_date"].ToString();
-
-                Console.WriteLine("Первый в базе id = {0} login = {1} password = {2} mail = {3} reg_date = {4}", id, login, password, mail, reg_date);
-
-                conn.Close();
-
-            }
-            catch
-            {
-                Console.WriteLine("bad");
-            }
-        }
         public void SignUp(string login, string password, string mail)
         {
             lock (key)
@@ -116,7 +55,6 @@ namespace Server.DB
                     return res == null;
                 }
             }
-
         }
         public bool CheckFreeLogin(string login)
         {
