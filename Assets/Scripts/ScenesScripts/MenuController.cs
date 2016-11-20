6fefea42f682bcdc1639916a1e7520087237738e
Boykooo <Boykooo@bk.ru>
2016-11-21 02:01:32 +0300

    Добавил меню входа в игру

---
+++ Assets/Scripts/ScenesScripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    public GUISkin q;
+    public Texture signInTexture;
+
+    private void Update()
+    {
+
+    }
+    private void OnGUI()
+    {
+        GUI.skin = q;
+        if (GUI.Button(new Rect(200, 200, 100, 100), signInTexture))
+        {
+
+        }
+    }
+}
