d62591c8d59d40af8d4397612b2751bd8d5d0df0
Boykooo <Boykooo@bk.ru>
2016-11-22 19:45:56 +0300

    Снизил версию .NET до 3.5  у Proxy, Network

---
+++ Assets/Scripts/Client/ClientFacade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Proxy;
+
+namespace Assets.Scripts.Client
+{
+    public class ClientFacade : IClientFacade
+    {
+        public void EnterTheGame(string login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorSignIn(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorSignUp(string error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SuccessfulSignUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
