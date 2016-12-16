63dfd842907b77242011da1fabc605f52fb7cf96
Полина <paulina.shevlyakova@yandex.ru>
2016-12-16 23:24:35 +0300

    Добавила картинку к меню битвы

--- Assets/Scripts/ScenesScripts/Battle.cs
+++ Assets/Scripts/ScenesScripts/Battle.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Battle : MonoBehaviour {
     public bool showMenu;
     public GUISkin Menu;
+    public Texture2D picture;
     // Use this for initialization
     void Start () {
         showMenu = false;
@@ -27,6 +29,9 @@ public class Battle : MonoBehaviour {
         {
                 GUI.Box(new Rect(Screen.width / 2 - 250, Screen.height / 2 - 150, 500, 300), "Битва"); //Создаем окно с меню 
                 GUI.Label(new Rect(Screen.width / 2-200, Screen.height / 2-100,400,200),"Уровень сложности битвы " + 10);
+                GUI.DrawTexture(new Rect(Screen.width / 2 -140, Screen.height / 2 -115, 280, 250),picture, ScaleMode.ScaleToFit, true);
+
+
             if (GUI.Button(new Rect(Screen.width / 2 - 180, Screen.height / 2 + 100, 180, 30), "Вступить в битву"))
             {
                
